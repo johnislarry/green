d3d75fa1737e50f89d9161ce1878e182dc69f4e7
sim finny <simfinny@sims-MBP.home>
2016-03-27 16:59:46 -0400

    playsinewave

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var portAudio = require('portaudio');
 var registerStdin = require('./registerListener').registerStdin;
 var toChar = require('./utils').toChar;
 var toOrd = require('./utils').toOrd;
+var playSineWave = require('./playsinewave').play;
 var MAPPED_KEYS = {
 	'q': 'c',
 	'2': 'c#',
@@ -23,7 +23,6 @@ var MAPPED_KEYS = {
 };
 
 function main() {
-  //playSineWave('G');
 	var subject = registerStdin();
 	var subscription = subject.subscribe(function(ord) {
 	  if (ord === 3) {
@@ -31,63 +30,10 @@ function main() {
 	    process.exit();
 	  }
 	  if (MAPPED_KEYS.hasOwnProperty(toChar(ord))) {
-	    // Do something....
+	    playSineWave(MAPPED_KEYS[toChar(ord)]);
 	  }
 	  console.log(MAPPED_KEYS[toChar(ord)]);
 	});
 }
-
-var playSineWave = function(note, octave)
-{
-    const factor = Math.pow(2, (1/12));
-    var sampleRate = 44100;
-
-    var notes = {
-        'a' : 0,
-        'a#': 1,
-        'b' : 2,
-        'c' : 3,
-        'c#': 4,
-        'd' : 5,
-        'd#': 6,
-        'e' : 7,
-        'f' : 8,
-        'f#': 9,
-        'g' : 10,
-        'g#': 11
-    };
-  
-    var tableSize = 44100/(440*Math.pow(factor,notes[note]));
-
-    var buffer = new Buffer(tableSize);
-    for (var i = 0; i < tableSize; i+= 1) {
-        buffer[i] = (Math.sin((i / tableSize) * 3.1415927 * 2.0) * 127);
-    }
-
-    portAudio.getDevices(function(err, devices) {
-        console.log(devices);
-    });
-
-    portAudio.open({
-        channelCount: 1,
-        sampleFormat: portAudio.SampleFormat8Bit,
-        sampleRate: sampleRate
-    }, function (err, pa) {
-        // send samples to be played
-        for (var i = 0; i < 5 * sampleRate / tableSize; i++) {
-        pa.write(buffer);
-    }
-
-    // start playing
-    pa.start();
-
-    // stop playing 1 second later
-    setTimeout(function () {
-      pa.stop();
-    }, 1 * 1000);
-  });
-};
-
-
 
 main();
---
+++ playsinewave.js
@@ -0,0 +1,47 @@
+var portAudio = require('portaudio');
+module.exports.play = function(note, octave)
+{
+    const factor = Math.pow(2, (1/12));
+    var sampleRate = 44100;
+
+    var notes = {
+        'a' : 0,
+        'a#': 1,
+        'b' : 2,
+        'c' : 3,
+        'c#': 4,
+        'd' : 5,
+        'd#': 6,
+        'e' : 7,
+        'f' : 8,
+        'f#': 9,
+        'g' : 10,
+        'g#': 11
+    };
+  
+    var tableSize = 44100/(440*Math.pow(factor,notes[note]));
+
+    var buffer = new Buffer(tableSize);
+    for (var i = 0; i < tableSize; i+= 1) {
+        buffer[i] = (Math.sin((i / tableSize) * 3.1415927 * 2.0) * 127);
+    }
+
+    portAudio.open({
+        channelCount: 1,
+        sampleFormat: portAudio.SampleFormat8Bit,
+        sampleRate: sampleRate
+    }, function (err, pa) {
+        // send samples to be played
+        for (var i = 0; i < 5 * sampleRate / tableSize; i++) {
+        pa.write(buffer);
+    }
+
+    // start playing
+    pa.start();
+
+    // stop playing 1 second later
+    setTimeout(function () {
+      pa.stop();
+    }, 1 * 1000);
+  });
+};
